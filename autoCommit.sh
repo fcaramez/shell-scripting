474708e910190453e17cc8e9f1c956bb8a07afad
fcaramez <francisco.caramez.duarte@gmail.com>
2022-04-15 09:12:16 +0100

    removes DS_STORE

--- autoCommit.sh
+++ autoCommit.sh
@@ -2,6 +2,6 @@
 
 echo ">>>>> Input your commit message and branch to push the changes"
 wait 2
-git add . && git commit -m "{$1}" && git push origin $2
+git add . && git commit -m "$1" && git push origin $2
 wait 2
 echo ">>>>> Success!"
