7e266170d302d82c9a7d6100da1105d8274c2760
fcaramez <francisco.caramez.duarte@gmail.com>
2022-04-15 14:38:34 +0100

    adds changes

--- getWeather.sh
+++ getWeather.sh
@@ -1,4 +1,4 @@
-#! /bin/zsh
+#! /bin/
 
 city=$1
 
--- tax.sh
+++ tax.sh
@@ -1,36 +1,6 @@
-#! node
+#! /bin/zsh
 
-const readline = require("readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+echo "Input your anual gross salary: "
+read $1
 
-readline.question("What's your anual income? ", (yearlyIncome) => {
-  switch (yearlyIncome) {
-    case yearlyIncome <= 7112:
-      yearlyIncome /= 1.145;
-      break;
-    case yearlyIncome <= 10732:
-      yearlyIncome /= 1.23;
-      break;
-    case yearlyIncome <= 20322:
-      yearlyIncome /= 1.28;
-      break;
-    case yearlyIncome <= 25075:
-      yearlyIncome /= 1.35;
-      break;
-    case yearlyIncome <= 36967:
-      yearlyIncome /= 1.37;
-      break;
-    case yearlyIncome <= 80882:
-      yearlyIncome /= 1.45;
-      break;
-    case yearlyIncome > 80882:
-      yearlyIncome /= 1.48;
-      break;
-  }
-
-  yearlyIncome = Math.floor(yearlyIncome / 1.11 / 14);
-  console.log("Your monthly salary is: ", yearlyIncome);
-  readline.close();
-});
+echo $1
